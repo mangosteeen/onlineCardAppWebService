8c6291d56ccb04123aa7ca33631d56874a45c6bf
mangosteeen <24045829@myrp.edu.sg>
2026-01-07 10:22:08 +0800

    typo commit

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.listen(port, () => {
 });
 
 // Example Route: Get all cards
-app.get('/cards', async (req, res) => {
+app.get('/allcards', async (req, res) => {
     try {
         let connection = await mysql.createConnection(dbConfig);
         const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
